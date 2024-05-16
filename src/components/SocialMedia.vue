30f42a533cb1ccf904cf69c0f81cbc49012d1031
Abdullah Mustapha <a41622100@gmail.com>
2024-05-16 01:38:45 +0000

    chore: refactor the icons with labels

--- src/components/SocialMedia.vue
+++ src/components/SocialMedia.vue
@@ -1,8 +1,25 @@
 <template>
     <div class="social_media_icon">
-        <a href="https://t.co/t7X93cilTF" target="_blank"><ion-icon name="logo-github"></ion-icon></a>
-        <a href="https://twitter.com/abdullah4tech_" target="_blank"><ion-icon name="logo-twitter"></ion-icon></a>
-        <a href="https://www.linkedin.com/in/abdullah-mustapha-ab529229a" target="_blank"><ion-icon name="logo-instagram"></ion-icon></a>
+        <a href="https://t.co/t7X93cilTF" target="_blank">
+          <ion-icon name="logo-github"></ion-icon>
+          <p>GitHub</p>
+        </a>
+        <a href="https://twitter.com/abdullah4tech_" target="_blank">
+          <ion-icon name="logo-twitter"></ion-icon>
+          <p>Twitter</p>
+        </a>
+        <a href="https://www.linkedin.com/in/abdullah-mustapha-ab529229a" target="_blank">
+          <ion-icon name="logo-instagram"></ion-icon>
+          <p>Instagram</p>
+        </a>
+        <a href="https://www.linkedin.com/in/abdullah-mustapha-ab529229a" target="_blank">
+          <ion-icon name="logo-linkedin"></ion-icon>
+          <p>Linkedin</p>
+        </a>
+        <a href="mailto:abdullahmu4life@gmail.com">
+          <ion-icon name="mail-outline"></ion-icon>
+          <p>Gmail</p>
+        </a>
     </div>
 </template>
 
@@ -12,25 +29,45 @@
     gap: 20px;
 }
 
-ion-icon {
-  color: white;
-  font-size: 30px;
-  transition: color .5s ease-in-out;
+ion-icon{
+  color: rgb(185, 183, 183);;
+  
+  transition: color .3s ease-in-out;
+  margin-bottom: 1px;
+}
+
+p{
+  margin-bottom: 1px;
 }
 
 ion-icon:hover {
-  color: rgb(185, 183, 183);
-  font-size: 30px;
+  color: white;
 }
 
 .social_media_icon a{
   border-bottom: 5px solid transparent; 
-  transition: border-bottom 0.3s ease; 
+  transition: 0.3s ease;
+  border-bottom: 1px solid #5151CA;
+  text-decoration: none;
+  color: rgb(185, 183, 183);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 5px;
+  font-size: 16px;
+}
+
+.social_media_icon{
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  gap: 10px;
 }
 
 .social_media_icon a:hover{
   border-radius: 5px;
-  border-bottom: 5px solid #5151CA;
 }
+
+
 
 </style>
