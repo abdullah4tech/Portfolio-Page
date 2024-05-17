93cbd3b0d421c2c13f6a9963f3bc5a634f35aabb
Abdullah Mustapha <a41622100@gmail.com>
2024-05-17 12:41:22 +0000

    chore: structed the layout

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,11 +1,12 @@
 <script setup>
 import SocialMedia from './SocialMedia.vue';
+import PageWrapper from '@/components/PageWrapper.vue'
 </script>
 
 <template>
-  <main>
+  <section class="Hero">
     <div class="container">
-      <div class="content show">
+      <div class="content">
         <h4>CREATE WEBSITE LIKE A PRO</h4>
         <h1>Hi, I'm <span>Abdullah</span>, Web Developer</h1>
         <p>
@@ -17,16 +18,90 @@ import SocialMedia from './SocialMedia.vue';
         </div>
       </div>
       <div class="image">
-        <img src="/src/assets/creative1.png">
+        <svg viewBox="0 0 1273 906" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g>
+            <path class="path"
+              d="M318.587 315.483V510.936L477.544 431.391V236.949M318.587 315.483L159.63 236.949M318.587 315.483L477.544 236.949M318.587 315.483V509.925L159.63 589.469M318.587 315.483L159.63 237.286L0.673828 315.483M318.587 315.483L159.63 394.016M477.544 236.949L318.587 158.753L159.63 236.949M477.544 236.949V432.402M477.544 236.949L318.587 158.416L477.544 80.2192L636.5 158.416L477.544 236.949ZM159.63 236.949V79.5451M159.63 589.469V394.016M159.63 589.469L0.673828 509.925V315.483M0.673828 315.483L159.63 394.016M159.63 79.5451L318.587 1.34863L477.544 79.5451L318.587 158.079L159.63 79.5451ZM795.457 395.701V237.286M795.457 237.286L636.5 159.09L477.544 237.286M795.457 237.286L636.5 315.82M477.544 237.286V431.728L601.139 491.762M477.544 237.286L636.5 315.82M636.5 315.82V472.887M954.414 668.003V473.561M954.414 473.561L795.457 395.364L636.5 473.561L795.457 552.094M954.414 473.561L795.457 552.094M954.414 473.561V669.014M954.414 473.561L795.457 395.027L954.414 316.831L1113.37 395.027L954.414 473.561ZM795.457 552.094L636.5 473.898L477.544 552.094M795.457 552.094L636.5 630.628M477.544 552.094V746.534L636.5 826.078V630.628M477.544 552.094L636.5 630.628M795.457 709.498V904.949M795.457 709.498L636.5 630.965M795.457 709.498L954.414 630.965M795.457 904.949L954.414 825.404V630.965M795.457 904.949L636.5 825.404V630.965M636.5 630.965L795.457 552.768L954.414 630.965M1113.37 552.431V747.882M1113.37 552.431L954.414 473.898M1113.37 552.431L1272.33 473.898M1113.37 747.882L1272.33 668.34V473.898M1113.37 747.882L954.414 668.34V473.898M954.414 473.898L1113.37 395.701L1272.33 473.898"
+              stroke="url(#paint-linear)" stroke-opacity="0.6" stroke-width="2"></path>
+          </g>
+          <path
+            d="M154 586C154 583.239 156.239 581 159 581V581C161.761 581 164 583.239 164 586V586C164 588.761 161.761 591 159 591V591C156.239 591 154 588.761 154 586V586Z"
+            fill="#27B173"></path>
+          <path
+            d="M154 393C154 390.239 156.239 388 159 388V388C161.761 388 164 390.239 164 393V393C164 395.761 161.761 398 159 398V398C156.239 398 154 395.761 154 393V393Z"
+            fill="#27B173"></path>
+          <path
+            d="M788 551C788 548.239 790.239 546 793 546V546C795.761 546 798 548.239 798 551V551C798 553.761 795.761 556 793 556V556C790.239 556 788 553.761 788 551V551Z"
+            fill="#27B173"></path>
+          <path
+            d="M1108 551C1108 548.239 1110.24 546 1113 546V546C1115.76 546 1118 548.239 1118 551V551C1118 553.761 1115.76 556 1113 556V556C1110.24 556 1108 553.761 1108 551V551Z"
+            fill="#27B173"></path>
+          <path
+            d="M1108 398C1108 395.239 1110.24 393 1113 393V393C1115.76 393 1118 395.239 1118 398V398C1118 400.761 1115.76 403 1113 403V403C1110.24 403 1108 400.761 1108 398V398Z"
+            fill="#27B173"></path>
+          <path
+            d="M788 237C788 234.239 790.239 232 793 232V232C795.761 232 798 234.239 798 237V237C798 239.761 795.761 242 793 242V242C790.239 242 788 239.761 788 237V237Z"
+            fill="#27B173"></path>
+          <defs>
+            <linearGradient id="paint-linear" x1="1272.23" y1="479.474" x2="506.242" y2="-216.277"
+              gradientUnits="userSpaceOnUse">
+              <stop stop-color="#27b173"></stop>
+              <stop offset="0.619553" stop-color="#1a663f"></stop>
+              <stop offset="0.93102" stop-color="#26312d"></stop>
+            </linearGradient>
+            <clipPath>
+              <rect width="1273" height="906" fill="white"></rect>
+            </clipPath>
+          </defs>
+        </svg>
       </div>
     </div>
-  </main>
+  </section>
+  <section class="Experience">
+    <h2>Work Experience</h2>
+    <div class="Work-Ex">
+      <div class="work">
+        <div class="logo">
+          <div class="img">
+            <img src="/src/assets/nexi_img.jpg" alt="code.png">
+          </div>
+        </div>
+        <div class="info">
+          <div class="child">
+            <h3>CODE</h3>
+            <p>Assisting Lecturer</p>
+          </div>
+          <time>JANUARY 5, 2024 - <span>PRESENT</span></time>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In aspernatur aliquid quas consectetur dignissimos unde nobis, ratione et omnis. Deserunt qui, eos modi eveniet beatae saepe quia culpa distinctio praesentium non similique quos impedit veritatis quod aperiam iusto at sint.
+          </p>
+        </div>
+      </div>
+      <div class="work">
+        <div class="logo">
+          <div class="img">
+            <img src="/src/assets/hng_img.jpg" alt="code.png">
+          </div>
+        </div>
+        <div class="info">
+          <div class="child">
+            <h3>CODE</h3>
+            <p>Assisting Lecturer</p>
+          </div>
+          <time class="time">JANUARY 5, 2024 - <span>PRESENT</span></time>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum repellendus quos dignissimos. Ad ab recusandae pariatur doloremque exercitationem numquam maiores. Aspernatur doloribus numquam accusamus quisquam voluptate velit unde perferendis corporis.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped>
 .content {
   max-width: 1200px;
-  padding: 3.5rem;
+  padding: 2.5rem;
   flex-shrink: 1;
   margin-top: 20;
 }
@@ -42,8 +117,7 @@ import SocialMedia from './SocialMedia.vue';
   justify-content: center;
   align-items: center;
   margin-top: 30px;
-  width: 80em;
-  overflow: hidden;
+  width: 84em;
 }
 
 .content {
@@ -58,7 +132,7 @@ import SocialMedia from './SocialMedia.vue';
   color: #ccc;
   font-size: 1rem;
   font-weight: 600;
-  animation: fadeIn 4s forwards; 
+  animation: fadeIn 4s forwards;
 }
 
 .container .content h1 {
@@ -67,7 +141,7 @@ import SocialMedia from './SocialMedia.vue';
   font-size: 3rem;
   font-weight: 700;
   line-height: 4rem;
-  animation: fadeIn 4s forwards; 
+  animation: fadeIn 4s forwards;
 }
 
 .container .content h1 span {
@@ -78,23 +152,21 @@ import SocialMedia from './SocialMedia.vue';
 .container .content p {
   margin-bottom: 2rem;
   color: #ccc;
-  animation: fadeIn 4s forwards; 
+  animation: fadeIn 4s forwards;
 }
 
 .image {
-  width:80em;
+  width: 80em;
 }
 
 .image img {
   width: 33em;
 }
 
-
-main {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+svg {
+  margin-left: 150px;
+  width: 450px;
+  height: 100%;
 }
 
 .medias {
@@ -103,13 +175,86 @@ main {
   animation: fadeIn 4s forwards;
 }
 
-@keyframes fadeIn {
-  from {
-    opacity: 0.3; /* Start opacity */
-  }
-  to {
-    opacity: 1; /* End opacity */
-  }
+.Experience{
+  max-width: 1300px;
+  margin: 0 auto;
+  padding: 20px;
 }
 
+.Experience h2{
+  margin-top: 50px;
+  margin-bottom: 50px;
+  color: white;
+  margin-right: 40px;
+}
+
+.info{
+  display: flex;
+  flex-direction: column;
+  gap: 7px;
+}
+
+.child{
+  display: flex;
+  flex-direction: column;
+  gap: 3px;
+}
+
+.info time{
+  color: #8d8d8d;
+}
+
+.info h3,
+p{
+  color: white;
+}
+
+.info time span{
+  color: rgb(12 206 107);
+}
+
+.Work-Ex{
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: auto;
+  margin-left: 35px;
+}
+
+.work{
+  display: flex;
+  justify-content: center;
+  gap: 30px;
+  width: 35em;
+}
+
+.img{
+  padding: 9px;
+  display: flex;
+  border: 1px solid #1F2937;
+}
+
+.img img{
+  width: 40px;
+}
+
+.logo 
+
+.info, p{
+  margin-bottom: 10px;
+}
+time{
+  padding-bottom: px;
+}
+
+@keyframes fadeIn {
+  from {
+    opacity: 0.3;
+    /* Start opacity */
+  }
+
+  to {
+    opacity: 1;
+    /* End opacity */
+  }
+}
 </style>
