2c15f8fe7015b1433a1c1d44569b5c76ce263ae5
Abdullah Mustapha <a41622100@gmail.com>
2024-05-26 18:46:41 +0000

    feat: updated the links on about page

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -23,7 +23,7 @@
         <h2>Soft Skills</h2>
         <ul>
           <li><b>Problem Solving: </b><p class="p"></p></li>
-          <li><b>Team Collaboration: </b>I thrive in team settings, communicating effectively and contributing to collective success.</li>
+          <li><b>Team Collaboration: </b><p class="p"></p>I thrive in team settings, communicating effectively and contributing to collective success.</li>
           <li><b>Adaptability: </b>I quickly learn and adapt to new technologies and environments, ensuring I stay up-to-date with industry trends.</li>
           <li><b>Attention to Detail: </b>I derive satisfaction from crafting meticulous designs and user interfaces, placing a strong emphasis on quality to ensure a polished end product.</li>
         </ul>
@@ -38,58 +38,58 @@
             <a href="https://vuejs.org">
               <p>Vue</p>
             </a>
-            <p> - JavaScript Frontend Framework</p>
+            <p class="p"> - JavaScript Frontend Framework</p>
           </li>
           <li>
             <i class="devicon-javascript-plain colored"></i>
-            <a href="https://vuejs.org">
+            <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript#:~:text=JavaScript%20(JS)%20is%20a%20lightweight,Apache%20CouchDB%20and%20Adobe%20Acrobat.">
               <p>JavaScript</p>
             </a>
-            <p> - Scripting Programming Language</p>
+            <p class="p"> - Scripting Programming Language</p>
           </li>
           <li>
             <i class="devicon-microsoftsqlserver-plain colored"></i>
-            <a href="https://vuejs.org">
+            <a href="https://www.google.com/search?q=microsoft+sql+server&oq=microsoft+sql+server&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCDgxODJqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8">
               <p>Microsoft SQL Server</p>
             </a>
-            <p> - Database Management System Application</p>
+            <p class="p"> - Database Management System Application</p>
           </li>
           <li>
             <svg viewBox="0 0 128 128">
               <linearGradient id="python-original-a" gradientUnits="userSpaceOnUse" x1="70.252" y1="1237.476" x2="170.659" y2="1151.089" gradientTransform="matrix(.563 0 0 -.568 -29.215 707.817)"><stop offset="0" stop-color="#5A9FD4"></stop><stop offset="1" stop-color="#306998"></stop></linearGradient><linearGradient id="python-original-b" gradientUnits="userSpaceOnUse" x1="209.474" y1="1098.811" x2="173.62" y2="1149.537" gradientTransform="matrix(.563 0 0 -.568 -29.215 707.817)"><stop offset="0" stop-color="#FFD43B"></stop><stop offset="1" stop-color="#FFE873"></stop></linearGradient><path fill="url(#python-original-a)" d="M63.391 1.988c-4.222.02-8.252.379-11.8 1.007-10.45 1.846-12.346 5.71-12.346 12.837v9.411h24.693v3.137H29.977c-7.176 0-13.46 4.313-15.426 12.521-2.268 9.405-2.368 15.275 0 25.096 1.755 7.311 5.947 12.519 13.124 12.519h8.491V67.234c0-8.151 7.051-15.34 15.426-15.34h24.665c6.866 0 12.346-5.654 12.346-12.548V15.833c0-6.693-5.646-11.72-12.346-12.837-4.244-.706-8.645-1.027-12.866-1.008zM50.037 9.557c2.55 0 4.634 2.117 4.634 4.721 0 2.593-2.083 4.69-4.634 4.69-2.56 0-4.633-2.097-4.633-4.69-.001-2.604 2.073-4.721 4.633-4.721z" transform="translate(0 10.26)"></path><path fill="url(#python-original-b)" d="M91.682 28.38v10.966c0 8.5-7.208 15.655-15.426 15.655H51.591c-6.756 0-12.346 5.783-12.346 12.549v23.515c0 6.691 5.818 10.628 12.346 12.547 7.816 2.297 15.312 2.713 24.665 0 6.216-1.801 12.346-5.423 12.346-12.547v-9.412H63.938v-3.138h37.012c7.176 0 9.852-5.005 12.348-12.519 2.578-7.735 2.467-15.174 0-25.096-1.774-7.145-5.161-12.521-12.348-12.521h-9.268zM77.809 87.927c2.561 0 4.634 2.097 4.634 4.692 0 2.602-2.074 4.719-4.634 4.719-2.55 0-4.633-2.117-4.633-4.719 0-2.595 2.083-4.692 4.633-4.692z" transform="translate(0 10.26)"></path><radialGradient id="python-original-c" cx="1825.678" cy="444.45" r="26.743" gradientTransform="matrix(0 -.24 -1.055 0 532.979 557.576)" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#B8B8B8" stop-opacity=".498"></stop><stop offset="1" stop-color="#7F7F7F" stop-opacity="0"></stop></radialGradient><path opacity=".444" fill="url(#python-original-c)" d="M97.309 119.597c0 3.543-14.816 6.416-33.091 6.416-18.276 0-33.092-2.873-33.092-6.416 0-3.544 14.815-6.417 33.092-6.417 18.275 0 33.091 2.872 33.091 6.417z"></path>
             </svg> 
-            <a href="https://vuejs.org">
+            <a href="https://www.python.org/">
               <p>Python</p>
             </a>
-            <p> - Programming Language</p>
+            <p class="p"> - Programming Language</p>
           </li>
           <li>
             <i class="devicon-c-plain colored"></i>
-            <a href="https://vuejs.org">
+            <a href="https://www.learn-c.org/">
               <p>C</p>
             </a>
-            <p> - Programming Language</p>
+            <p class="p"> - Programming Language</p>
           </li>
           <li>
             <i class="devicon-cplusplus-plain colored"></i>
-            <a href="https://vuejs.org">
+            <a href="https://isocpp.org/">
               <p>C++</p>
             </a>
-            <p> - Programming Language</p>
+            <p class="p"> - Programming Language</p>
           </li>
           <li>
             <i class="devicon-tailwindcss-original colored"></i>
-            <a href="https://vuejs.org">
+            <a href="https://tailwindcss.com/">
               <p>Tailwind CSS</p>
             </a>
-            <p> - CSS UI Framework</p>
+            <p class="p"> - CSS UI Framework</p>
           </li>
           <li>
             <i class="devicon-laravel-original colored"></i>
-            <a href="https://vuejs.org">
+            <a href="https://laravel.com/">
               <p>Laravel</p>
             </a>
-            <p> - PHP Backend Framework</p>
+            <p class="p"> - PHP Backend Framework</p>
           </li>
         </ul>
       </div>
@@ -98,31 +98,31 @@
         <ul>
           <li>
             <i class="devicon-git-plain colored"></i>
-            <a href="https://vuejs.org">
+            <a href="https://git-scm.com/">
               <p>Git</p>
             </a>
-            <p> - JavaScript Frontend Framework</p>
+            <p class="p"> - Local version control system</p>
           </li>
           <li>
             <i class="devicon-github-original colored"></i>
-            <a href="https://vuejs.org">
+            <a href="https://github.com">
               <p>GitHub</p>
             </a>
-            <p> - JavaScript Frontend Framework</p>
+            <p class="p"> - Remote version control system</p>
           </li>
           <li>
             <i class="devicon-vscode-plain colored"></i>
-            <a href="https://vuejs.org">
+            <a href="https://code.visualstudio.com/">
               <p>Visual Studio Code</p>
             </a>
-            <p> - JavaScript Frontend Framework</p>
+            <p class="p"> - Code Editor</p>
           </li>
           <li>
             <i class="devicon-postman-plain colored"></i> 
-            <a href="https://vuejs.org">
+            <a href="https://www.postman.com/">
               <p>Postman</p>
             </a>
-            <p> - JavaScript Frontend Framework</p>
+            <p class="p"> - API testing platform</p>
           </li>
         </ul>
       </div>
@@ -214,8 +214,7 @@ h1 {
   margin-bottom: 40px;
 }
 
-.p{
-  margin-bottom: 20px;
+.p, .technologies .p, .tools .p{
   color: grey;
 }
 
@@ -316,12 +315,12 @@ h2, b, .technologies p, .tools p{
 }
 
 .technologies a p{
-  color: rgb(70, 70, 224);
+  color: white;
   font-weight: bold;
 }
 
 .tools a p{
-  color: rgb(70, 70, 224);
+  color: white;
   font-weight: bold;
 }
 
