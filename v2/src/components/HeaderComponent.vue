889e34f6e8a8b3ea4bf6e72cb755d3397298403d
abdullah4tech <a41622100@gmail.com>
2025-02-01 23:55:26 +0000

    feat: started designing

---
+++ v2/src/components/HeaderComponent.vue
@@ -0,0 +1,22 @@
+<script setup lang='ts'></script>
+
+
+<template>
+  <header class="flex justify-between py-4 px-15">
+
+    <!-- Logo -->
+    <div>
+      <span>Abdullah.</span>
+    </div>
+
+    <!-- Navigation -->
+    <nav class=" text-black py-2 px-4 flex items-center justify-center rounded-full">
+      <ul class="flex items-center gap-8">
+        <li><a href="#">About</a></li>
+        <li><a href="#">Work</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+
+  </header>
+</template>
