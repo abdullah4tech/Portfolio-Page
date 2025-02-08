50d24bc2ab84a044ad010712ce92e6699450250e
abdullah4tech <a41622100@gmail.com>
2025-02-08 00:28:01 +0000

    feat: updated links on the landing page

--- v2/src/App.vue
+++ v2/src/App.vue
@@ -3,10 +3,22 @@ import HeaderComponent from './components/HeaderComponent.vue';
 import { ArrowRight, Twitter, Github, MailIcon, ArrowDownFromLine } from "lucide-vue-next";
 
 const projects = [
-  "Authentication System",
-  "Product Landing Page",
-  "Backend Starter CLI",
-  "Community Extension",
+  {
+    name: "Authentication System",
+    link: "https://auth-system-gr5y.onrender.com/",
+  },
+  {
+    name: "Backend Starter CLI",
+    link: "https://www.npmjs.com/package/backend-starter-cli/",
+  },
+  {
+    name: "Community Extension",
+    link: "https://github.com/abdullah4tech/community-extension",
+  },
+  {
+    nane: "Product Landing Page",
+    link: "https://e-commerce-product-page-three-pi.vercel.app/",
+  },
 ];
 </script>
 
@@ -21,15 +33,15 @@ const projects = [
           <p class="text-2xl italic font-light">I'm a dedicated Software Engineer focused on building scalable web applications and intuitive user interfaces, turning complex challenges into innovative solutions.</p>
         </div>
         <div class="flex items-center gap-5 mt-10">
-          <span class="p-3 bg-gray-800/10 backdrop-blur-md rounded-full">
+          <a href="https://twitter.com/abdullah4tech_" target="_blank" class="p-3 bg-gray-800/10 backdrop-blur-md rounded-full">
             <Twitter />
-          </span>
-          <span class="p-3 bg-gray-800/10 backdrop-blur-md rounded-full">
+          </a>
+          <a href="https://github.com/abdullah4tech" target="_blank" class="p-3 bg-gray-800/10 backdrop-blur-md rounded-full">
             <Github />
-          </span>
-          <span class="p-3 bg-gray-800/10 backdrop-blur-md rounded-full">
+          </a>
+          <a href="mailto:abdullahmu4life@gmail.com" class="p-3 bg-gray-800/10 backdrop-blur-md rounded-full">
             <MailIcon />
-          </span>
+          </a>
           <div class="flex items-center px-3 py-2 bg-gray-800/10 backdrop-blur-md rounded-full gap-4">
             <span>Resume</span>
             <span class="p-2 bg-black rounded-full">
@@ -44,21 +56,22 @@ const projects = [
           <li
             v-for="(project, index) in projects"
             :key="index"
-            class="relative py-3 flex items-center justify-between group overflow-hidden cursor-pointer border-b border-gray-400"
           >
-            <!-- Border Fill Effect -->
-            <span
-              class="absolute bottom-0 left-0 w-full h-[2px] bg-gray-500 origin-left scale-x-0 transition-transform duration-500 group-hover:scale-x-100"
-            ></span>
+            <a :href="project.link" target="_blank" class="relative py-3 flex items-center justify-between group overflow-hidden cursor-pointer border-b border-gray-400">
+              <!-- Border Fill Effect -->
+              <span
+                class="absolute bottom-0 left-0 w-full h-[2px] bg-gray-500 origin-left scale-x-0 transition-transform duration-500 group-hover:scale-x-100"
+              ></span>
 
-            <span>{{ project }}</span>
+              <span>{{ project.name }}</span>
 
-            <!-- Arrow Appears on Hover -->
-            <span
-              class="transform scale-0 opacity-0 transition-all duration-300 group-hover:scale-100 group-hover:opacity-100 group-hover:translate-x-0 translate-x-4"
-            >
-              <ArrowRight class="w-5 h-5 text-gray-500" />
-            </span>
+              <!-- Arrow Appears on Hover -->
+              <span
+                class="transform scale-0 opacity-0 transition-all duration-300 group-hover:scale-100 group-hover:opacity-100 group-hover:translate-x-0 translate-x-4"
+              >
+                <ArrowRight class="w-5 h-5 text-gray-500" />
+              </span>
+            </a>
           </li>
         </ul>
       </div>
---
+++ v2/src/components/CardComponent.vue
@@ -0,0 +1,12 @@
+<script setup lang='ts'>
+
+</script>
+
+
+<template>
+  <section>
+    <div></div>
+    <div></div>
+    <div></div>
+  </section>
+</template>
