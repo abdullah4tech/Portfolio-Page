95cbbffffd737eae3a6a0cfea732629fff4e58c2
Abdullah Mustapha <a41622100@gmail.com>
2025-12-05 11:22:44 +0000

    feat: add VueUse Motion plugin for page animations

--- v2/src/main.ts
+++ v2/src/main.ts
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createHead } from '@vueuse/head'
+import { MotionPlugin } from '@vueuse/motion'
 import App from './App.vue'
 import router from './router'
 
@@ -10,6 +11,7 @@ const head = createHead()
 
 app.use(head)
 app.use(router)
+app.use(MotionPlugin)
 
 // Ensure DOM is ready before mounting
 app.mount('#app')
